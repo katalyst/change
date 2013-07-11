302305a36d1d6608a6ff58114382193140c2a8ec
Haydn Ewers <haydn@particlesystem.com>
2013-07-11 22:41:18 +0930

    Updated the SHA1 for both formulas.

--- change.rb
+++ change.rb
@@ -4,7 +4,7 @@ class Change < Formula
 
   homepage 'https://github.com/katalyst/change'
   url 'https://github.com/katalyst/change/zipball/v1.3.0'
-  sha1 '3212677aeb50293da04656ec37108164a2e2a549'
+  sha1 'cd14f8688f07f7089929f3c1d48a27035485f295'
 
   def test
     system "change"
