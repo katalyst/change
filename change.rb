b22b2a90b8268917b5c64d3db61970d34d5af276
Haydn Ewers <haydn@particlesystem.com>
2013-07-11 22:38:57 +0930

    Update the install URL to reflect the new location.

--- change.rb
+++ change.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Change < Formula
 
-  homepage 'https://github.com/ketchup/change'
-  url 'https://github.com/ketchup/change/zipball/v1.3.0'
+  homepage 'https://github.com/katalyst/change'
+  url 'https://github.com/katalyst/change/zipball/v1.3.0'
   sha1 '3212677aeb50293da04656ec37108164a2e2a549'
 
   def test
